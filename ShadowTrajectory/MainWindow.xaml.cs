490b32ee9daa338e888bf28f2ad5ab7e51f2626a
masrcull <masrcull@gmail.com>
2023-09-30 11:20:23 -0600

    Shadow trajecotry shadow selection

--- ShadowTrajectory/MainWindow.xaml.cs
+++ ShadowTrajectory/MainWindow.xaml.cs
@@ -36,10 +36,16 @@ namespace ShadowTrajectory
             MainStage.Children.Add(GCC);
 
             GCC.CreateCircleModel("C:\\ModelExports\\circley.json", .2, 128, 0, 0);
-            GCC.CreateTriangleModel("C:\\ModelExports\\triangley.json", 1, 0, 128, 0);
+            GCC.CreateTriangleModel("C:\\ModelExports\\triangley.json", .2, 0, 128, 0);
 
             var TriangleMan = new Model("C:\\ModelExports\\triangley.json", 0, 0, 15);
             CircleShadowModel = new Model("C:\\ModelExports\\circley.json", 0, 0, 15);
+            CircleShadowModel.Scale(1, 1, 1);
+            CircleShadowModel.SetColor(33, 33, 33);
+            CircleShadowModel.RotateX(-Math.PI / 2);
+            CircleShadowModel.RotateX(Math.PI / 8);
+            CircleShadowModel.RotateY(Math.PI / 8);
+
             CircleModel = new Model("C:\\ModelExports\\circley.json", 0, 1, 15);
             var tableBottom = new Model("..\\..\\..\\..\\ModelRotate\\Models\\cube_model.json", 0, 0, 15);
             var tableSideRightFront = new Model("..\\..\\..\\..\\ModelRotate\\Models\\cube_model.json", 2.0, -1.1, 14.2);
@@ -49,7 +55,7 @@ namespace ShadowTrajectory
 
 
             //CircleModel.Scale(3,3,3);
-            CircleShadowModel.Scale(1, 1, 1);
+            
 
             tableSideLeftFront.Scale(0.3, 2, .2);
             tableSideLeftBack.Scale(0.3, 2, .2);
@@ -65,55 +71,28 @@ namespace ShadowTrajectory
 
             var table = GCC.AddModels( new List<Model> { tableSideRightFront, tableSideRightBack, tableSideLeftFront, tableSideLeftBack, tableBottom } );
             var shadow = GCC.AddModels(new List<Model> { CircleShadowModel });
-            //var triangleMan = GCC.AddModels(new List<Model> { TriangleMan });
-
-            table.SetColorAllModels(66, 66, 66);
-            CircleShadowModel.SetColor(33, 33, 33);
-
-            //var table = GCC.AddModels(new List<Model> { tableSideRight, tableSideLeft, tableBottom });
-
-            //var table = GCC.AddModels(new List<Model> {  CircleModel, CircleModel2 });
-
             var circleMesh = GCC.AddModels(new List<Model> { CircleModel });
 
-            //tableBottom.RotateX(10);
-            //tableSideRight.RotateX(10);
+            table.SetColorAllModels(66, 66, 66);
 
+            
 
-
-            //circleMesh.RotateY(3.14);
-
-            bool redIncrease = true;
-            bool greenIncrease = true;
-            bool blueIncrease = true;
 
             byte red = 64;
             byte green = 128;
             byte blue = 200;
 
-            CircleShadowModel.RotateX(-Math.PI / 2);
+            
 
             table.RotateX(-Math.PI/8);
             table.RotateY(-Math.PI / 8);
-            CircleShadowModel.RotateX(Math.PI / 8);
-            CircleShadowModel.RotateY(Math.PI / 8);
+            
             //table.RotateX(-.6);
 
 
             GCC.AddMethod(() =>
             {
-                red = ColorHelper.IncrementRgbByte(red, (byte)4, ref redIncrease);
-                blue = ColorHelper.IncrementRgbByte(blue, (byte)16, ref blueIncrease);
-                green = ColorHelper.IncrementRgbByte(green, (byte)8, ref greenIncrease);
 
-                //tableBottom.color = new int[] { red, green, blue };
-
-                //circles.RotateX(.1);
-
-                //table.RotateY(.1);
-                //CircleModel.RotateY(.1);
-
-                //tableBottom.RotateX(.1);
             });
 
             GCC.Start();
@@ -156,6 +135,38 @@ namespace ShadowTrajectory
             //CircleModel.Translate(-centroid[0], -centroid[1], -centroid[2]);
             //CircleModel.Translate(e.NewValue, 0, 0);
             //CircleModel.Translate(centroid[0], centroid[1], centroid[2]);
+        }
+
+        private void OnChecked(object sender, RoutedEventArgs e)
+        {
+
+
+            GCC.Meshes.RemoveAt(2);
+            GCC.Meshes.RemoveAt(1);
+
+
+
+            RadioButton checkedRadioButton = sender as RadioButton;
+
+            if (checkedRadioButton != null)
+            {
+                switch (checkedRadioButton.Content)
+                {
+                    case "Disc":
+                        CircleShadowModel = new Model("C:\\ModelExports\\circley.json", CircleShadowModel.Centroid[0], CircleShadowModel.Centroid[1], CircleShadowModel.Centroid[2]);
+                        break;
+                    case "Triangle":
+                        CircleShadowModel = new Model("C:\\ModelExports\\triangley.json", CircleShadowModel.Centroid[0], CircleShadowModel.Centroid[1], CircleShadowModel.Centroid[2]);
+                        break;
+                }
+                CircleShadowModel.Scale(1, 1, 1);
+                CircleShadowModel.SetColor(33, 33, 33);
+                CircleShadowModel.RotateX(-Math.PI / 2);
+                CircleShadowModel.RotateX(Math.PI / 8);
+                CircleShadowModel.RotateY(Math.PI / 8);
+                GCC.AddModels(new List<Model> { CircleShadowModel });
+                GCC.AddModels(new List<Model> { CircleModel });
+            }
         }
 
     }
